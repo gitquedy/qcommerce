757c891225fb2d2907099c54888d88eb23d73244
Cainta-PC-2-Gabriel <41713299+gabbbcapili@users.noreply.github.com>
2019-11-29 17:24:38 +0800

    update redirect_uri

--- app/Lazop.php
+++ app/Lazop.php
@@ -8,7 +8,8 @@ class Lazop extends Model
 {
 
     public static function getAuthLink(){
-    	$redirect_uri = 'https://lazada.yuukihost.com/shop/form'; // route('shop.form')
+    	// $redirect_uri = 'https://lazada.yuukihost.com/shop/form'; 
+        $redirect_uri =  route('shop.form'); 
     	return 'https://auth.lazada.com/oauth/authorize?response_type=code&force_auth=true&redirect_uri='. $redirect_uri .'&client_id=' . env('lazada_app_key');
     }
     
