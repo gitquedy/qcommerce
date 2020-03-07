09eb77726ed73b3804f22fe1b83ea155a5dc354a
Cainta-PC-Gabriel <41713299+gabbbcapili@users.noreply.github.com>
2020-03-07 17:25:49 +0800

    forces ssl

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -1,12 +1,19 @@
 <?php
 
-namespace MyApp\Http\Middleware;
+namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
 
-class HttpsProtocol {
-
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
             if (!$request->secure() && App::environment() === 'production') {
