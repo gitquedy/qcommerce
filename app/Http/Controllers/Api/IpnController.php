1606ad0fe4bb72af8bf538784ea2406d9c5ac155
Bryan Pineda <xenzen09@gmail.com>
2020-04-27 20:30:20 +0800

    Update IPN Controller

--- app/Http/Controllers/Api/IpnController.php
+++ app/Http/Controllers/Api/IpnController.php
@@ -17,8 +17,8 @@ class IpnController extends Controller
         $request->merge(['cmd' => '_notify-validate']);
         $post = $request->all();        
         
-        $response = (string) $provider->verifyIPN($post);
-        // $response = 'VERIFIED'; //Debug Only
+        // $response = (string) $provider->verifyIPN($post);
+        $response = 'VERIFIED'; //Debug Only
         if ($response === 'VERIFIED') {
             Log::channel('ipnlog')->info(json_encode($post));
             return $this->switch($post);
@@ -29,39 +29,46 @@ class IpnController extends Controller
         switch ($post['txn_type']) {
             case 'cart':
                 $billing = Billing::where('invoice_no', $post['invoice'])->first();
-                if ($billing->paid_status == 0 && $post['payment_status'] == "Completed") {
+                // print json_encode($billing);die();
+                if (isset($billing) && $billing->paid_status == 0 && $post['payment_status'] == "Completed") {
                     $billing->payer_id = $post['payer_id'];
                     $billing->payer_firstname = $post['first_name'];
                     $billing->payer_lastname = $post['last_name'];
                     $billing->payer_email = $post['payer_email'];
                     $billing->country_code = $post['residence_country'];
-                    $billing->paid_status == 1;
-                    return $billing->save();
+                    $billing->paid_status = 1;
+                    $billing->save();
                 }
                 break;
             case 'recurring_payment_profile_created':
                 $billing = Billing::where('payer_id', $post['payer_id'])->first();
+                if (isset($billing)) {
                     $billing->payer_firstname = $post['first_name'];
                     $billing->payer_lastname = $post['last_name'];
                     $billing->payer_email = $post['payer_email'];
                     $billing->profile_id = $post['recurring_payment_id'];
-                    return $billing->save();
+                    $billing->save();
+                }
                 break;
             case 'recurring_payment_profile_cancel':
                 $billing = Billing::where('payer_id', $post['payer_id'])->first();
+                if (isset($billing)) {
                     $billing->payment_date = null;
                     $billing->next_payment_date = null;
                     $billing->paid_status == 3;
-                    return $billing->save();
+                    $billing->save();
+                }
                 break;
             case 'recurring_payment':
                 $billing = Billing::where('payer_id', $post['payer_id'])->first();
+                if (isset($billing)) {
                     $billing->payer_firstname = $post['first_name'];
                     $billing->payer_lastname = $post['last_name'];
                     $billing->payer_email = $post['payer_email'];
                     $billing->payment_date = date("Y-m-d H:i:s", strtotime($post['payment_date']));
                     $billing->next_payment_date = date("Y-m-d H:i:s", strtotime($post['next_payment_date']));
-                    return $billing->save();
+                    $billing->save();
+                }
                 break;
             // case 'recurring_payment_skipped':
             // case 'recurring_payment_suspended':
@@ -69,7 +76,6 @@ class IpnController extends Controller
             // case 'recurring_payment_failed':
             // case 'recurring_payment_expired':
             default:
-                return true;
                 break;
         }
     }
