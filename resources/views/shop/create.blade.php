d2cd86de4b19d5d80114dc2f81bd323418899715
Cainta-PC-Gabriel <41713299+gabbbcapili@users.noreply.github.com>
2020-09-07 12:25:22 +0800

    shop bug

--- resources/views/shop/create.blade.php
+++ resources/views/shop/create.blade.php
@@ -117,7 +117,7 @@
           }
           window.location.href = $('#channel').find(":selected").data('url') + '?channel=' + domain;
         }
-        window.location.href = $('#channel').find(":selected").data('url') + '?channel=' + $("#domain").val();
+        window.location.href = $('#channel').find(":selected").data('url');
       });
       $('select[name=channel]').on('change', function() {
         if(this.value == 'shopify'){
