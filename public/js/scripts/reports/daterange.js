367c8cef0fa89cacb086ca0e6e4c6efe5297b6ee
Francis Ian Aquino <37167282+francisianaquino@users.noreply.github.com>
2021-09-03 13:15:35 +0800

    Added 'All' option on daterangepicker.js

--- public/js/scripts/reports/daterange.js
+++ public/js/scripts/reports/daterange.js
@@ -17,7 +17,8 @@ $(function() {
            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
            'This Month': [moment().startOf('month'), moment().endOf('month')],
-           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+           'All': [moment().set('year', 2000).startOf('year'), moment()]
         }
     }, cb);
 
